4550c9e78afd607c5e59288d0bdfc41a1b1c4972
gianlu <gianlu.giudice@gmail.com>
2019-06-30 18:54:21 +0200

    Variables refactoring

--- src/parking/ticketing/BookingTicket.java
+++ src/parking/ticketing/BookingTicket.java
@@ -6,19 +6,19 @@ import parking.ParkingSpot;
 import java.util.Objects;
 
 public class BookingTicket extends Ticket {
-    private int startSlice;
+    private int startTimeSlice;
 
-    public BookingTicket(int parkID, ParkingSpot carParkedSlot, Car carParked, int startSlice) {
+    public BookingTicket(int parkID, ParkingSpot carParkedSlot, Car carParked, int startTimeSlice) {
         super(parkID, carParkedSlot, carParked);
-        this.startSlice = startSlice;
+        this.startTimeSlice = startTimeSlice;
     }
 
-    public int getStartSlice() {
-        return startSlice;
+    public int getStartTimeSlice() {
+        return startTimeSlice;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), startSlice);
+        return Objects.hash(super.hashCode(), startTimeSlice);
     }
 }
